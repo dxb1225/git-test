da862ab052fa63dc5bc5b620f37d069057812dac
SouthWind <southWind@163.com>
2023-02-08 09:31:11 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @Author:邓湘标
+ * @Package:com.atguigu.git
+ * @DESCRIPTION:
+ * @DATE: 2023/2/8 9:26
+ * @Version:1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
